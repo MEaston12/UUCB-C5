ff631a2ab7b8ca835203c53f47818de17eb9a04e
Malcolm Easton <eastonmalcolm@gmail.com>
2021-04-18 20:14:53 -0600

    added saving functionality, minor refactor of init

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //Using JQuery 3.4.1 and Luxon 1.26.0
 
-const now = luxon.DateTime.now();
+let now = luxon.DateTime.now();
+let timeBlocks = [];
 
 $('#currentDay').text(`Today is ${now.toLocaleString({
     weekday: 'long',
@@ -8,28 +9,62 @@ $('#currentDay').text(`Today is ${now.toLocaleString({
     day: '2-digit'
 })}`);
 
-const timeBlocks = [];
-for(let i = 6; i < 20; i++){ //Begin/end of range set here
-    timeBlocks.push({
-        lux: now.set({hour: i, minute:0, second:0})
-    });
-}
-
 function renderTimeBlocks(){
-    const runTime = luxon.DateTime.now();
+    now = luxon.DateTime.now();
     $('#time-blocks').empty();
     for(let timeBlock of timeBlocks){
         $('#time-blocks').append(`
-<div class="input-group time-block" id='${timeBlock.lux.hour}-time-block'>
+<div class="input-group time-block" data-hr="${timeBlock.lux.hour}">
     <div class="input-group-prepend">
         <span class="input-group-text">${timeBlock.lux.toLocaleString(luxon.DateTime.TIME_SIMPLE)}</span>
     </div>
-    <input type="text" class="form-control ${timeBlock.lux > runTime ? 'future' : timeBlock.lux.plus({hours:1}) > runTime ? 'present' : 'past'}" placeholder="No event this hour, click to add event.">
+    <input type="text" class="form-control ${timeBlock.lux > now ? 'future' : timeBlock.lux.plus({hours:1}) > now ? 'present' : 'past'}" placeholder="No event this hour, click to add event.">
     <div class="input-group-append">
-    <button class="btn btn-primary" type="button">SAVE</button>
+        <button class="btn btn-primary saveBtn" type="button">
+            <i class="fas fa-save"></i>
+        </button>
     </div>
 </div>`)
     }
+    $('input').each((_index, element) =>{
+        const timeBlockEle = $(element).closest('.time-block');
+        const targetHr = timeBlockEle.data('hr');
+        const timeBlock = timeBlocks.find(ele => ele.lux.hour === targetHr);
+        timeBlockEle.find('input').val(timeBlock.eventStr);
+    });
+    $('.saveBtn').click((e) => {
+        const timeBlockEle = $(e.target).closest('.time-block')
+        const targetHr = timeBlockEle.data('hr');
+        const timeBlock = timeBlocks.find(ele => ele.lux.hour === targetHr);
+        timeBlock.eventStr = timeBlockEle.find('input').val();
+        saveToStorage();
+    });
 }
 
+function initializeTimeBlocksArr(){
+    for(let i = 6; i < 24; i++){ //Begin/end of range set here
+        timeBlocks.push({
+            lux: now.set({hour: i, minute:0, second:0}),
+            eventStr: ''
+        });
+    }
+    saveToStorage();
+}
+
+function pullFromStorage(){
+    timeBlocks = JSON.parse(localStorage.getItem('timeBlocks'));
+    timeBlocks.forEach((e) => {
+            e.lux = luxon.DateTime.fromISO(e.lux);
+        });
+}
+
+function saveToStorage(){
+    localStorage.setItem('timeBlocks',JSON.stringify(timeBlocks));
+}
+
+if(localStorage.getItem('timeBlocks')){
+    pullFromStorage();
+} else {
+    initializeTimeBlocksArr();
+}
 renderTimeBlocks();
